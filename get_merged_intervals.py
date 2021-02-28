0640a89857eed17c18c59cc26fd381059f828d6d
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-02-28 11:59:36 +0530

    Update get_merged_intervals.py

--- get_merged_intervals.py
+++ get_merged_intervals.py
@@ -4,23 +4,18 @@ class Solution:
             return intervals
 
         intervals.sort(key=lambda i: i[0])
-
         output = []
-
         start, end = intervals[0]
 
         for i in range(1, len(intervals)):
             star, en = intervals[i]
-
             if star <= end:
-
                 end = max(end, en)
             else:
                 output.append([start, end])
                 start, end = star, en
-
         output.append([start, end])
         return output
         
 s = Solution()
-print(s.getMergedIntervals([[7,7],[2,3],[6,11],[1,2]]))
+print(s.getMergedIntervals([[7,7],[2,3],[6,11],[1,2]])) # [[1, 3], [6, 11]]
